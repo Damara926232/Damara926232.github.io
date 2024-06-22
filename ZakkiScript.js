4d70c285dd3138f825b225602d5b49f044e4b966
Zakki Zhao <130901583+Damara926232@users.noreply.github.com>
2024-06-23 01:38:34 +0800

    Delete ZakkiScript.js

--- ZakkiScript.js
+++
@@ -1,7 +0,0 @@
-
-function toggleMenu() {
-    var menuWrap = document.querySelector('.menu-wrap');
-    var hamburger = document.querySelector('.c-hamburger');
-    hamburger.classList.toggle('is-active');
-    menuWrap.classList.toggle('show-menu');
-};
